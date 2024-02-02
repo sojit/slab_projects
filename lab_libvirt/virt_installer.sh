cf7a144c1738d991c32e046b620f392b18e00cbe
sojit <sojithomas88@gail.com>
2024-02-02 19:23:29 +0530

    corrected the virt installer script comments

--- lab_libvirt/virt_installer.sh
+++ lab_libvirt/virt_installer.sh
@@ -17,16 +17,18 @@ VM_CPU="$3"
 BASE_DISK_SIZE="$4"
 NETWORK_NAME="kub-network"  
 
+
+# Debian qcow2 image path and cloud-init ISO path
 deb_qcow2_path="https://cloud.debian.org/images/cloud/buster/20190909-10/debian-10-generic-amd64-20190909-10.qcow2"
 cloud_init="~/Documents/kvm_storage/seed-init.iso"
 qcow_name=$(basename "$deb_qcow2_path")
 
+# Paths for storing images and VMs
 image_storage_path="~/Documents/disk_images"
-
 vm_storage_path="~/Documents/kvm_storage"
 
 
-#move the cloud image to the correct directory path
+#move the cloud image to the correct local storage directory path
 vm_name="${VM_NAME}_$qcow_name"
 function image_copy () {
      cp "$image_storage_path/$qcow_name"  "$vm_storage_path/$vm_name"
@@ -34,17 +36,19 @@ function image_copy () {
 
 #check if the file exists
 if [[ ! -f "$image_storage_path/$qcow_name" ]]; then
+    # If the file does not exist, download it, then copy to the desired location
     if wget  "$deb_qcow2_path" -O "$image_storage_path/$qcow_name" ; then
     echo "File downloaded successfully!"
     image_copy
-    fi
-    
+    fi    
 elif [[ -f "$image_storage_path/$qcow_name" ]] && [[ ! -f "$vm_storage_path/$vm_name" ]]; then
-     image_copy
+    # If the image file exists but the VM file does not, copy the image 
+    image_copy
     
 fi
 
- 
+
+#create vm based on the provided arguments
 virt-install  \
    --name "$VM_NAME" \
    --memory "$VM_MEMORY" \
